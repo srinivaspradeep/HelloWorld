6077c57e288321c266013471df6b8d8c45b4d49d
srinivaspradeep <e.srinivaspradeep@gmail.com>
2014-07-19 11:44:18 +0530

    Adding github project
    Initial commit of all the project files

---
+++ github/github/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("hello git");
+
+        }
+    }
+}
